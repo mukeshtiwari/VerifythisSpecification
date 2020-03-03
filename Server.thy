293a58727f08000a9ee7179a3326e6b597ab96b3
Toby Murray <toby.murray@unimelb.edu.au>
2020-03-03 17:15:23 +1100

    add invariant to Isabelle model and example proof of preservation

--- Server.thy
+++ Server.thy
@@ -28,6 +28,10 @@ definition fresh :: "utoken \<Rightarrow> state \<Rightarrow> bool" where
 definition upload_pre :: "key \<Rightarrow> state \<Rightarrow> bool" where
   "upload_pre k s \<equiv> finger k \<in> dom (keys s) \<longrightarrow> the ((keys s) (finger k)) = k"
 
+lemma upload_pre_def2:
+  "upload_pre k s = (case ((keys s) (finger k)) of Some k' \<Rightarrow> k' = k | None \<Rightarrow> True)"
+  by(auto simp: upload_pre_def split: option.splits)
+
 definition upload_post :: "key \<Rightarrow> state \<Rightarrow> state \<Rightarrow> bool" where
   "upload_post k s s' \<equiv> \<exists> t. fresh t s \<and> 
    s' = (s\<lparr>keys := (keys s)((finger k) \<mapsto> k),
@@ -37,6 +41,30 @@ definition upload_post :: "key \<Rightarrow> state \<Rightarrow> state \<Rightar
 definition upload :: "key \<Rightarrow> state \<Rightarrow> state \<Rightarrow> bool" where
   "upload k s s' \<equiv>  if upload_pre k s then upload_post k s s' else s' = s"
 
-(* TODO: other operations :) *)
+text {* Gidon's invariant -- are there any other invariants we should encode? *}
+definition inv :: "state \<Rightarrow> bool" where
+  "inv s \<equiv> (\<forall>f \<in> dom (keys s). finger (the ((keys s) f)) = f) \<and>
+           (ran (uploaded s) \<subseteq> (dom (keys s))) \<and>
+           (\<forall>(f,i) \<in> ran (pending s). f \<in> dom (keys s) \<and> i \<in> ids (the (keys s f))) \<and>
+           (\<forall>i f. (confirmed s) i = Some f \<longrightarrow>  f \<in> dom (keys s) \<and> i \<in> ids (the (keys s f))) \<and>
+           (ran (managed s) \<subseteq> (dom (keys s)))"
+
+(* super uninteresting proof -- how to automate? *)
+lemma upload_inv:
+  "\<lbrakk>inv s; upload k s s'\<rbrakk> \<Longrightarrow> inv s'"
+  apply(clarsimp simp: upload_def inv_def simp: upload_pre_def upload_post_def split: if_splits)
+  apply(rule conjI)
+  apply(clarsimp simp: upload_def inv_def simp: upload_pre_def upload_post_def split: if_splits simp: ran_def dom_def)
+   apply blast
+  apply(rule conjI)
+  apply(clarsimp simp: upload_def inv_def simp: upload_pre_def upload_post_def split: if_splits simp: ran_def dom_def)
+   apply fastforce
+  apply(rule conjI)
+  apply(clarsimp simp: upload_def inv_def simp: upload_pre_def upload_post_def split: if_splits simp: ran_def dom_def)
+   apply fastforce
+  apply(clarsimp simp: upload_def inv_def simp: upload_pre_def upload_post_def split: if_splits simp: ran_def dom_def)
+  apply blast
+  done
+
 
 end
